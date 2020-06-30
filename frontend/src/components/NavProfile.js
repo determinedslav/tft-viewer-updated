1c4a6f47c2f6eb1b88f9b9df416b4f1ded90e53e
DeterminedSlav <slavimitev10@gmail.com>
2020-06-30 20:01:24 +0300

    added find friend stats

--- frontend/src/components/NavProfile.js
+++ frontend/src/components/NavProfile.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {useSelector, useDispatch} from "react-redux";
 import { useHistory } from "react-router-dom";
+import DynamicSort from '../components/js/DynamicSort';
+import {setStats} from '../redux/actions/stats';
+import {setPlayer} from '../redux/actions/player';
+import {setLoading} from '../redux/actions/loading';
+import {setMatch} from '../redux/actions/match';
+import {setMatchIndex} from '../redux/actions/matchIndex';
 import {setLoggedUser} from '../redux/actions/loggedUser';
+import RiotAPIManager from '../network/riot-api';
 
 const NavProfile = props => {
 
     const loggedUserUsername = useSelector(state => state.loggedUser.username);
     const loggedUserFriends = useSelector(state => state.loggedUser.friends);
+
+    const isLoading = useSelector(state => state.loading);
 
     const dispatch = useDispatch();
     const history = useHistory();
@@ -32,6 +41,46 @@ const NavProfile = props => {
         history.push('/')
     }
 
+    const getRegionCode = (value) => {
+        switch(value) {
+            case 'EUNE':
+                return'eun1';
+            case 'EUW':
+                return'euw1';
+            default:
+              return 'Error';
+          }
+    };
+
+    const loadFriend = async (friend) => {
+        const regionCode = getRegionCode(friend.region);
+        dispatch(setLoading(true));
+        if (!isLoading) {
+            const responsePlayer = await RiotAPIManager.getPlayer(friend.name, regionCode, friend.region);
+            if(responsePlayer && responsePlayer.hasOwnProperty('newPlayer')){
+                dispatch(setPlayer(responsePlayer.newPlayer));
+                dispatch(setStats(responsePlayer.newStats));
+                const responseMatches = await RiotAPIManager.getMatches(responsePlayer.newPlayer);
+                handleMatches(responseMatches);
+            } else {
+                dispatch(setLoading(false));
+            }
+        }
+    }
+
+    const handleMatches = (matches) => {
+        setTimeout(()=>{
+            if (matches.length !== 0) {
+                matches.sort(DynamicSort.sortMatches("dateTime"));
+                console.log(matches);
+                dispatch(setMatch(matches));
+                dispatch(setMatchIndex(' '));
+                history.push('/match');
+            }
+            dispatch(setLoading(false));
+        },2000); 
+    }
+
     return <div>
             {loggedUserUsername !== undefined ? 
             <div>
@@ -42,7 +91,7 @@ const NavProfile = props => {
                     <select id="selectFriend" defaultValue = "0" className="form-control-sm">
                         <option value="0" disabled>Saved players</option>
                         {loggedUserFriends.map((friend, i) => {
-                            return <option key={i} value={friend}>{friend.name + "#" + friend.region}</option>
+                            return <option key={i} value={friend} onClick = {() => loadFriend(friend)}>{friend.name + "#" + friend.region}</option>
                         })}
                     </select>
                 </div>
